86b575e7dabfc0df0ca7f8a89bf55349b7248f5e
redelasi <806137926@qq.com>
2019-09-02 01:57:06 +0800

    集成spring gateway。

---
+++ springcloud-garteway/src/main/java/com/springcloud/gateway/SpringCloudGateWay.java
@@ -0,0 +1,17 @@
+package com.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: mingxing gong
+ * @since: 2019/9/2 01:38
+ */
+
+@SpringBootApplication
+public class SpringCloudGateWay {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGateWay.class,args);
+    }
+}
