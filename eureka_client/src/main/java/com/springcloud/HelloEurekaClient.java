d84d0ce39afccb070f795e61b609d1255de72af8
redelasi <806137926@qq.com>
2019-09-01 17:36:01 +0800

    添加spring cloud bus模块。

--- eureka_client/src/main/java/com/springcloud/HelloEurekaClient.java
+++ eureka_client/src/main/java/com/springcloud/HelloEurekaClient.java
@@ -3,6 +3,7 @@ package com.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -13,6 +14,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class HelloEurekaClient {
 
     public static void main(String[] args) {
